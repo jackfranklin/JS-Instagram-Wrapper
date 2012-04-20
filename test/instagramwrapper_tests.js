24853509aa6b1e8164d4b0852a6b3e0f0cf41fd1
Jack Franklin <jack@jackfranklin.net>
2012-04-20 14:47:15 +0100

    Tutorial 2 done, added in Mockjax

--- test/instagramwrapper_tests.js
+++ test/instagramwrapper_tests.js
@@ -8,11 +8,16 @@ test('oembed', function() {
   equal(instagramwrapper.oembed.web_url, "http://instagr.am/p/JYAdvJr0K9/", "Can set the web_url correctly");
 });
 asyncTest("oembed AJAX", 4, function() {
+  $.mockjax({
+    url: 'http://api.instagram.com/oembed?url=http://instagr.am/p/JYAdvJr0K9/',
+    contentType: 'text/json',
+    proxy: '/test/oembed.json'
+  });
   instagramwrapper.oembed.web_url = "http://instagr.am/p/JYAdvJr0K9/";
   instagramwrapper.oembed.query(function(res) {
     ok(res, "AJAX call got a result");
     ok(res.url, "URL exists in response");
-    equal(res.url, "http://distilleryimage5.instagram.com/9436051c85b011e18cf91231380fd29b_7.jpg", "URL returned is correct");
+    equal(res.url, "http://distilleryimage5.s3.amazonaws.com/9436051c85b011e18cf91231380fd29b_7.jpg", "URL returned is correct");
     equal(res.title, "Drainpipe", "The title returned is correct");
     start();
   });
